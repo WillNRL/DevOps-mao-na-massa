12db8a33428ba768912fe17cc5cc338cef94daaf
will <will@pop-os.localdomain>
2023-01-05 10:36:29 -0400

    att critica: control-node - vagrantfile e provision

--- 02-ansible-lab/control-node/provision.sh
+++ 02-ansible-lab/control-node/provision.sh
@@ -5,6 +5,7 @@ echo "alias python='python3'" >> .bashrc
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --user
 python3 -m pip install --user ansible
+echo ""
 cat <<EOT >> /etc/hosts
 192.168.1.2 control-node
 192.168.1.3 app01
