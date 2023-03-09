fc21176efc2c70f1d499a86e07a8eddc5acf94f6
Your Name <you@example.com>
2023-03-09 14:12:52 -0400

    att: ajuste do provision e vagrantfile

--- 02-ansble-lab/provision/node.sh
+++ 02-ansble-lab/provision/node.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 echo "Inicio da instalacao do ansible"
+sudo apt-get update
 sudo apt-get install -y python3 pip curl wget
-echo "alias python='python3'" >> .bashrc
+alias python='python3'
+sed -i -e 's/alias python='python3'/alias python='python3'/g' ~/.bashrc
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py --user
 python3 -m pip install --user ansible
-echo ""
 cat <<EOT >> /etc/hosts
 192.168.1.2 control-node
 192.168.1.3 app01
